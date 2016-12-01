69f288eb4e0e579bdadc95f20826ce095122ab98
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2016-12-02 00:51:20 +0100

    Refactoring

--- CSharp/Day01/Program.cs
+++ CSharp/Day01/Program.cs
@@ -10,31 +10,31 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             var result = 
-				Math.Abs(
-					(from part in File
-						.ReadAllText("Input1.txt")
-						.Split(',')
-					let mov = Regex.Match(part,"([LR])([0-9]*)")
-					where mov.Success
-					select new { 
-						Right = mov.Groups[1].Value == "R"?-1:1,
-						Blocks = int.Parse(mov.Groups[2].Value)
-					})
-					.Aggregate(
-						new 
-						{ 
-							IncX = 0, IncY = 1, // North
-							Distance = 0 
-						}, 
-						(acum, next) => 
-							new 
-							{ 
-								IncX = - acum.IncY * next.Right, 
-								IncY = acum.IncX * next.Right, 
-								Distance = acum.Distance + next.Right * next.Blocks * (acum.IncX - acum.IncY) 
-							}
-						)
-					.Distance);
+                Math.Abs(
+                    (from part in File
+                        .ReadAllText("Input1.txt")
+                        .Split(',')
+                    let mov = Regex.Match(part,"([LR])([0-9]*)")
+                    where mov.Success
+                    select new { 
+                        Direction = mov.Groups[1].Value == "R"?-1:1,
+                        Blocks = int.Parse(mov.Groups[2].Value)
+                    })
+                    .Aggregate(
+                        new 
+                        { 
+                            IncX = 0, IncY = 1, // North
+                            Distance = 0 
+                        }, 
+                        (acum, next) => 
+                            new 
+                            { 
+                                IncX = - acum.IncY * next.Direction, 
+                                IncY = acum.IncX * next.Direction, 
+                                Distance = acum.Distance + next.Direction * next.Blocks * (acum.IncX - acum.IncY) 
+                            }
+                        )
+                    .Distance);
 
             Console.WriteLine(result);
         }
