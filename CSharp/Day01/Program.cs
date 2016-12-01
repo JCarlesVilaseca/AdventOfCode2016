f7a3884620bb229035fd96be65017b05cc412a20
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2016-12-01 17:20:30 +0100

    First commit before finishing first day

---
+++ CSharp/Day01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = from part in File
+                                        .ReadAllText("Input1.txt")
+                                        .Split(',')
+                                        .Select(part => part.Trim())
+                            let left = Regex.Match(part,"L([0-9]*)")
+                            let right = Regex.Match(part,"R([0-9]*)")
+                            select new { 
+                                Left = left.Success?int.Parse(left.Groups[1].Value):0,
+                                Right = right.Success?int.Parse(right.Groups[1].Value):0
+                            };
+
+            var position = result.Aggregate(
+                new { IncX = 0, IncY = 0 }, 
+                (dir, src) => 
+                {
+                    return new { IncX = 1, IncY = 0 };
+                });
+
+            Console.WriteLine(position);
+        }
+    }
+}
