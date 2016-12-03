3f4cd88655c465e00965e20be4ff60511f4056f4
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2016-12-03 13:31:57 +0100

    Day 02 Part 1

--- CSharp/Day02/Program.cs
+++ CSharp/Day02/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -9,21 +10,35 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             var data = 
-				(from direction in "ULLRRDD"
-				select direction)
-				.Aggregate(
-					1,
-					(acum, next) => {
-						switch(next)
-						{
-							case 'U': return acum-3 < 1?acum:acum-3;
-							case 'D': return acum+3 > 9?acum:acum+3;
-							case 'U': return acum-3 < 1?acum:acum-3;
-							case 'D': return acum+3 > 9?acum:acum+3;
-						}
-					}
-				);
+                from line in File.ReadAllLines("Input1.txt")
+                select line;
 
+            var part1 =
+                data.Aggregate(
+                    (IEnumerable<int>)new[] { 4 },
+                    (list,line) => {
+                        var current = list.Last();
+                        var next = 
+                            line.Aggregate(
+                                current,
+                                (acum, direction) => {
+                                    switch(direction)
+                                    {
+                                        case 'U': return acum-3 < 0?acum:acum-3;
+                                        case 'D': return acum+3 > 8?acum:acum+3;
+                                        case 'L': return (acum%3)==0?acum:acum-1;
+                                        case 'R': return ((acum+1)%3)==0?acum:acum+1;
+                                    }
+                                    
+                                    return acum;
+                                }
+                            );
+                        return list.Concat(new [] { next });
+                    })
+                    .Select(x => x + 1) // My grid is base(0)
+                    .Skip(1); // Skip code seed
+                    
+            Console.WriteLine("Part 1: " +  string.Join("",part1)); 
         }
     }
 }
