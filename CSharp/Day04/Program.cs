3caf1f29bda963c49ba512ce46803466f7ecb2a1
JCarles Vilaseca <joan.carles.vilaseca@gmail.com>
2016-12-04 16:25:33 +0100

    Day 04 Part 2

--- CSharp/Day04/Program.cs
+++ CSharp/Day04/Program.cs
@@ -12,26 +12,38 @@ namespace ConsoleApplication
         {
             var data =
                 from line in File.ReadAllLines("Input1.txt")
-                let parts = Regex.Match(line, @"([a-z\-]*)([0-9]+)\[([a-z]+)\]")
+                let parts = Regex.Match(line, @"([a-z\-]*)\-([0-9]+)\[([a-z]+)\]")
                 where parts.Success
                 select new 
                 { 
-                    Name = parts.Groups[1].Value.Replace("-",""),
+                    Name = parts.Groups[1].Value,
                     SectorID = int.Parse(parts.Groups[2].Value),
                     Checksum = parts.Groups[3].Value
                 };
 
             var part1 =
                 from room in data
-                let orderedChars = from chr in room.Name
-                        group chr by chr into g
-                        orderby g.Count() descending, g.Key
-                        select g.Key
+                let orderedChars = 
+                    from chr in room.Name.Replace("-","")
+                    group chr by chr into g
+                    orderby g.Count() descending, g.Key
+                    select g.Key
                 let orderedString = string.Join("",orderedChars)
                 where orderedString.StartsWith(room.Checksum)
                 select room.SectorID;
 
             Console.WriteLine("Part 1: " + part1.Sum());
+
+            var part2 =
+                from room in data
+                let decryptedChars = 
+                    from chr in room.Name
+                    select chr=='-'?' ':Convert.ToChar(((((int)chr - (int)'a') + room.SectorID) % 26) + (int)'a')
+                let decrypted = string.Join("",decryptedChars)
+                where decrypted == "northpole object storage"
+                select room.SectorID;
+
+            Console.WriteLine("Part2: " + part2.FirstOrDefault());
         }
     }
 }
